f66b4af0b6df01139041222ea91983c9eb7e94b8
harrisgomez <node.harris@gmail.com>
2020-10-10 01:25:42 -0400

    Transform config line items to key value pairs.

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 const fs = require('fs');
 const pathToFile = '/example.config';
 
-const textToArr = text => {
-    return text.split('\n');
-};
-
 const parseConfigToJSON = () => {
+    // Read local file as a string
     const str = fs.readFileSync(__dirname + pathToFile, 'utf8', (err, data) => {
         if (err) throw err;
         return data;
     });
-    
-    const arr = textToArr(str);
+    const arrOfStrings = str.split(`\n`);   // Turn each line in the string into an array item
+    const obj = {};
 
-    console.log(arr);
+    arrOfStrings
+        .filter(line => line[0] !== '#')    // Creates a new array from items that do not begin with '#'
+        .map(str => str.split('='))         // Returns a new array containing keys and values
+        .forEach(keyValPair => {            // Iterate over new array of keys and values
+            let key = keyValPair[0].trim(); // Remove any whitespace from keys
+            let val = keyValPair[1].trim(); // Remove any whitespace from values
+
+            obj[key] = val;                 // Assign key-value pairs to object literal
+        })
+
+    console.log(obj);
 }
 
 parseConfigToJSON();
