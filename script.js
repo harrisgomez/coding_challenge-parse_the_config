737d7f038ee5a053ff193691ad2ff9f616793a88
harrisgomez <node.harris@gmail.com>
2020-10-10 03:52:18 -0400

    Added utility functions for Boolean and Number type coercion. Added use case examples for parsing plain-text config file to JSON.

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const fs = require('fs');
-const pathToFile = '/example.config';
+const config = '/example.config';
 
-const parseConfigToJSON = () => {
+// Coerces Boolean-like strings to Boolean true/false if the converted value matches any of the truthy/falsy items, respectively.
+const getBoolType = val => {
+    const isString = typeof val === 'string';
+    const truthy = ['true', 'on', 'yes'],
+          falsy = ['false', 'off', 'no'];
+
+    if (isString && truthy.some(t => val.includes(t))) return true;
+    if (isString && falsy.some(f => val.includes(f))) return false;
+
+    return val;
+}
+
+// Coerces numeric strings to type Number if the converted value does not equal NaN
+const getNumType = val => {
+    const isString = typeof val === 'string',
+          isNumeric = !isNaN(Number(val));
+
+    if (isString && isNumeric) return Number(val);
+
+    return val;
+}
+
+const parseConfigToJSON = file => {
     // Read local file as a string
-    const str = fs.readFileSync(__dirname + pathToFile, 'utf8', (err, data) => {
+    const str = fs.readFileSync(__dirname + file, 'utf8', (err, data) => {
         if (err) throw err;
         return data;
     });
@@ -11,16 +33,30 @@ const parseConfigToJSON = () => {
     const obj = {};
 
     arrOfStrings
-        .filter(line => line[0] !== '#')    // Creates a new array from items that do not begin with '#'
+        .filter(line => line[0] !== '#')    // Creates a new array from items, ignoring comments
         .map(str => str.split('='))         // Returns a new array containing keys and values
         .forEach(keyValPair => {            // Iterate over new array of keys and values
             let key = keyValPair[0].trim(); // Remove any whitespace from keys
             let val = keyValPair[1].trim(); // Remove any whitespace from values
 
-            obj[key] = val;                 // Assign key-value pairs to object literal
+            val = getBoolType(val);         // Coerce truthy/falsy values of type String to type Boolean
+            val = getNumType(val);          // Coerce numeric values of type String to type Number
+
+            obj[key] = val;                 // Create properties from key value pairs on the initialized object literal
         })
 
-    console.log(obj);
-}
+    return obj;
+};
 
-parseConfigToJSON();
+// Example use of the parseConfigToJSON function
+const actionableConfig = parseConfigToJSON(config);
+
+console.log(`Value of "host" is ${actionableConfig.host} and is of type ${typeof actionableConfig.host}`);
+console.log(`Value of "server_id" is ${actionableConfig.server_id} and is of type ${typeof actionableConfig.server_id}`);
+console.log(`Value of "server_load_alarm" is ${actionableConfig.server_load_alarm} and is of type ${typeof actionableConfig.server_load_alarm}`);
+console.log(`Value of "user" is ${actionableConfig.user} and is of type ${typeof actionableConfig.user}`);
+console.log(`Value of "verbose" is ${actionableConfig.verbose} and is of type ${typeof actionableConfig.verbose}`);
+console.log(`Value of "test_mode" is ${actionableConfig.test_mode} and is of type ${typeof actionableConfig.test_mode}`);
+console.log(`Value of "debug_mode" is ${actionableConfig.debug_mode} and is of type ${typeof actionableConfig.debug_mode}`);
+console.log(`Value of "log_file_path" is ${actionableConfig.log_file_path} and is of type ${typeof actionableConfig.log_file_path}`);
+console.log(`Value of "send_notifications" is ${actionableConfig.send_notifications} and is of type ${typeof actionableConfig.send_notifications}`);
